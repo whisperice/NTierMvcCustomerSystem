d8fdfb165731c7e94d0364ceb42d55045f8f0307
whisperice <dengyitaogm@gmail.com>
2019-11-05 15:27:34 +1100

    Construct N-tier MVC structure

---
+++ NTierMvcCustomerSystem.DataAccess/Common/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierMvcCustomerSystem.DataAccess.Common
+{
+    // This interface contains common methods implemented by all repository classes.
+    public interface IRepository<TEntity> where TEntity:class
+    {
+        bool Insert(TEntity entity);
+        bool Update(TEntity entity);
+        bool DeleteById(int id);
+        TEntity SelectById(int id);
+        List<TEntity> SelectAll();
+    }
+}
---
+++ NTierMvcCustomerSystem/Controllers/CustomersController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NTierMvcCustomerSystem.Model;
+
+namespace NTierMvcCustomerSystem.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return RedirectToAction("ListAll");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult ListAll()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, UserName = "User1"},
+                new Customer { Id = 2, UserName = "User2"}
+            };
+        }
+
+        public ActionResult AddNote()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(customer);
+        }
+    }
+}
